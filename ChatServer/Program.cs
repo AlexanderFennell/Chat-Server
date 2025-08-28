e69f77145453de57d8519b775abf60ef92d99d41
Alexander-Fennell <alexander@fennell.se>
2025-08-28 23:36:15 +0200

    Add project files.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    const string ipAddressString = "127.0.0.1";
+    const int port = 8000;
+
+    static void  Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse(ipAddressString);
+        TcpListener server = new TcpListener(ipAddress, port);
+        server.Start();
+        Console.WriteLine($"Server started.");
+
+    }
+
+    static void ClientJoined(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[8064];
+        while (true)
+        {
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine(message);
+
+            byte[] replyData = Encoding.UTF8.GetBytes(message);
+            stream.Write(replyData, 0, replyData.Length);
+        }
+    }
+}
