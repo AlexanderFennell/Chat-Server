de2282e250e995c333f35117f4e94ad9eccd759e
Alexander-Fennell <alexander@fennell.se>
2025-08-28 23:52:14 +0200

    Made it possible to connect to server with multiple clients and

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -14,21 +14,29 @@ class Program
         TcpListener server = new TcpListener(ipAddress, port);
         server.Start();
         Console.WriteLine($"Server started.");
-
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Task.Run(() => ClientJoined(client));
+        }
     }
 
     static void ClientJoined(TcpClient client)
     {
         NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[8064];
+        int bytesNameRead = stream.Read(buffer, 0, buffer.Length);
+        string userName = Encoding.UTF8.GetString(buffer, 0, bytesNameRead);
+        Console.WriteLine($"{userName} has connected to the server.");
+        
         while (true)
         {
             int bytesRead = stream.Read(buffer, 0, buffer.Length);
             string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
             Console.WriteLine(message);
-
             byte[] replyData = Encoding.UTF8.GetBytes(message);
             stream.Write(replyData, 0, replyData.Length);
+
         }
     }
 }
