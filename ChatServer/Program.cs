0d37513480d7edccd6ecfab331e002cc94e80707
Alexander-Fennell <alexander@fennell.se>
2025-09-11 20:44:05 +0200

    Made messages send and made the username list update

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -7,9 +7,11 @@ class Program
 {
     const string ipAddressString = "127.0.0.1";
     const int port = 8000;
+    static public List<string> userNameList = new List<string>();
 
     static void  Main()
     {
+        
         IPAddress ipAddress = IPAddress.Parse(ipAddressString);
         TcpListener server = new TcpListener(ipAddress, port);
         server.Start();
@@ -27,8 +29,10 @@ class Program
         byte[] buffer = new byte[8064];
         int bytesNameRead = stream.Read(buffer, 0, buffer.Length);
         string userName = Encoding.UTF8.GetString(buffer, 0, bytesNameRead);
+        userNameList.Add(userName);
         Console.WriteLine($"{userName} has connected to the server.");
-        
+        byte[] playerListBuffer = Encoding.UTF8.GetBytes(PushUserListToClients());
+        stream.Write(playerListBuffer, 0, playerListBuffer.Length);
         while (true)
         {
             int bytesRead = stream.Read(buffer, 0, buffer.Length);
@@ -36,7 +40,17 @@ class Program
             Console.WriteLine(message);
             byte[] replyData = Encoding.UTF8.GetBytes(message);
             stream.Write(replyData, 0, replyData.Length);
-
         }
+    }
+
+    static string PushUserListToClients()
+    {
+        string usernameListMessage = "/c";
+        foreach (string userName in userNameList) 
+        {
+            usernameListMessage += $"/n/{userName}";
+        }
+        return usernameListMessage;
+        
     }
 }
